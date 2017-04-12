0dee5b4cfa473b730b40e2feae1cffbccd4e9270
Laulaucia <buitrago.laura@gmail.com>
2017-04-12 00:52:03 -0700

    animations

--- js/init.js
+++ js/init.js
@@ -8,3 +8,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var boxes = document.getElementsByClassName("box");
+var triangles = document.getElementsByClassName('triangle');
+var blobs = document.getElementsByClassName('blob');
+TweenMax.staggerTo(boxes, 3, {x:800, y:200}, .2);
