4bfdc2c6c1c4fd981026221019fefc61582e1804
laulaucia <buitrago.laura@gmail.com>
2019-12-03 20:06:58 -0800

    more animations

--- js/init.js
+++ js/init.js
@@ -1,23 +1,19 @@
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-     var intro = gsap.timeline({}); 
-intro.from("#all-star", {scale: 200, duration: 2});
-intro.from("h1", {x: -100, duration: 1});
+  var intro = gsap.timeline({}).call(callStar); 
+  intro.from("#all-star", {scale: 200, duration: 2});
+  intro.from("h1", {x: -100, duration: 1});
+  intro.from(".grid section", {y: 100, duration: 1}, "<");
 
+  var starLine = gsap.timeline({ yoyo: true, repeat: 2, delay: 1}).reverse();
+  starLine.to("#seafoamStar", {rotation: 36, transformOrigin:"50% 50%", duration: 1}, "<");
+  starLine.fromTo("#pinkCircle",{scale:0, transformOrigin:"50% 50%"}, {scale: 1.1, duration: 2, x:0, y:0}, "<");
+  starLine.to("#whiteStar", {rotation: -396, duration: 2, transformOrigin:"50% 50%", ease: "expo"}, "<");
+  starLine.fromTo("#lavenderCircle",{scale:0, transformOrigin:"50% 50%"}, {scale: 1.1, duration: 1, x:0, y:0}, "<");
 
-var starLine = gsap.timeline({ yoyo: true, repeat: 2, delay: 1}).reverse();
-starLine.to("#seafoamStar", {rotation: 36, transformOrigin:"50% 50%", duration: 1}, "<");
-starLine.fromTo("#pinkCircle",{scale:0, transformOrigin:"50% 50%"}, {scale: 1.1, duration: 2, x:0, y:0}, "<");
-starLine.to("#whiteStar", {rotation: -396, duration: 2, transformOrigin:"50% 50%", ease: "expo"}, "<");
-starLine.fromTo("#lavenderCircle",{scale:0, transformOrigin:"50% 50%"}, {scale: 1.1, duration: 1, x:0, y:0}, "<");
-  
-document.getElementById("all-star").addEventListener("click",     function() {
-  console.log("clicked");
-  starLine.reversed(!starLine.reversed());
+  function callStar(){
+    starLine.reversed(!starLine.reversed());
+  }
+  document.getElementById("all-star").addEventListener("click", callStar);
+  M.AutoInit();
 });
-     M.AutoInit();
-    // var elems = document.querySelectorAll('.materialboxed');
-    // var instances = M.Materialbox.init(elems);
-    //     var paralaxelems = document.querySelectorAll('.parallax');
-    // var instances = M.Parallax.init(paralaxelems);
-  });
