133f645365b38b1550f81dd898be8671576f6c56
laulaucia <buitrago.laura@gmail.com>
2019-12-03 19:43:40 -0800

    no more jquery

--- js/init.js
+++ js/init.js
@@ -1,17 +1,23 @@
-(function($){
-  $(function(){
+  document.addEventListener('DOMContentLoaded', function() {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    var rows = document.getElementsByClassName("row-2");
-    for(var row of rows){row.closest(".material-placeholder").classList.add("row-2")};
-    
-    
+     var intro = gsap.timeline({}); 
+intro.from("#all-star", {scale: 200, duration: 2});
+intro.from("h1", {x: -100, duration: 1});
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
 
-var boxes = document.getElementsByClassName("box");
-var triangles = document.getElementsByClassName('triangle');
-var blobs = document.getElementsByClassName('blob');
-TweenMax.staggerTo(boxes, 3, {x:800, y:200}, .2);
+var starLine = gsap.timeline({ yoyo: true, repeat: 2, delay: 1}).reverse();
+starLine.to("#seafoamStar", {rotation: 36, transformOrigin:"50% 50%", duration: 1}, "<");
+starLine.fromTo("#pinkCircle",{scale:0, transformOrigin:"50% 50%"}, {scale: 1.1, duration: 2, x:0, y:0}, "<");
+starLine.to("#whiteStar", {rotation: -396, duration: 2, transformOrigin:"50% 50%", ease: "expo"}, "<");
+starLine.fromTo("#lavenderCircle",{scale:0, transformOrigin:"50% 50%"}, {scale: 1.1, duration: 1, x:0, y:0}, "<");
+  
+document.getElementById("all-star").addEventListener("click",     function() {
+  console.log("clicked");
+  starLine.reversed(!starLine.reversed());
+});
+     M.AutoInit();
+    // var elems = document.querySelectorAll('.materialboxed');
+    // var instances = M.Materialbox.init(elems);
+    //     var paralaxelems = document.querySelectorAll('.parallax');
+    // var instances = M.Parallax.init(paralaxelems);
+  });
